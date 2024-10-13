e64f9dac919893cb9bb341e2ba624e86b3e85d67
Karga <70997923+KargaEntiti@users.noreply.github.com>
2024-10-12 23:38:38 -0300

    Update Persona.java

--- src/main/java/com/turnos/turnos/Persona.java
+++ src/main/java/com/turnos/turnos/Persona.java
@@ -5,12 +5,14 @@ public abstract class Persona {
     protected int telefono;
     protected String apellido;
     protected String nombre;
+    protected String mail;
 
-    public Persona(int dni, int telefono, String nombre, String apellido) {
+    public Persona(int dni, int telefono, String nombre, String apellido, String mail) {
         this.dni = dni;
         this.telefono = telefono;
         this.nombre = nombre;
         this.apellido = apellido;
+        this.mail = mail;
     }
 
 }
