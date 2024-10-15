435a908a577646f38956849c78e773ed4d34c2a1
Ian Vidmar <68394559+DRVR1@users.noreply.github.com>
2024-10-15 11:30:07 -0300

    Token

---
+++ src/main/java/com/turnos/turnos/Security/JwtToPrincipalConverter.java
@@ -0,0 +1,26 @@
+package com.turnos.turnos.Security;
+
+import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class JwtToPrincipalConverter {
+    public UserPrincipal convert(DecodedJWT jwt){
+
+        Long id = Long.valueOf(jwt.getSubject());
+        String mail = jwt.getClaim("e").asString();
+        List<SimpleGrantedAuthority> authorities = extractAuthoritiesFromClaim(jwt);
+
+        return new UserPrincipal(id,mail,authorities);
+
+    }
+
+    private List<SimpleGrantedAuthority> extractAuthoritiesFromClaim(DecodedJWT jwt){
+        var claim = jwt.getClaim("a");
+        if (claim.isNull() || claim.isMissing()) return List.of();
+        return claim.asList(SimpleGrantedAuthority.class);
+    }
+}
--- src/main/java/com/turnos/turnos/Security/SecurityConfig.java
+++ src/main/java/com/turnos/turnos/Security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.turnos.turnos.Security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -7,14 +8,16 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
         http
                 .cors(co -> co
                         .disable())
@@ -29,8 +32,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests(registry -> registry
                         .requestMatchers("/").permitAll()
                         .requestMatchers("/auth/login").permitAll()
-                        .anyRequest().permitAll() //para desarrollo
-                        //.anyRequest().authenticated()
+                        //.anyRequest().permitAll() //para desarrollo
+                        .anyRequest().authenticated() // para produccion
                 )
 
                 .rememberMe(Customizer.withDefaults());
