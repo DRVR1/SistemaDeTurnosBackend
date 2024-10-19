8ade2f34d456015be7ecbfe501c6bfc4067e8fa3
Ian Vidmar <68394559+DRVR1@users.noreply.github.com>
2024-10-19 01:34:15 -0300

    popups con estilo y confirmacion para cancelar turno

--- src/main/resources/static/js/ver_mis_turnos.js
+++ src/main/resources/static/js/ver_mis_turnos.js
@@ -1,5 +1,4 @@
 
-document.getElementById('buscarBtn').addEventListener('click', buscarTurnos);
 
 async function buscarTurnos() {
     const pacienteId = localStorage.getItem('userId');
@@ -10,14 +9,30 @@ async function buscarTurnos() {
 
     turnosList.forEach(turno => {
         const li = document.createElement('li');
+        li.id = 'li'+turno.id;
         li.innerHTML = `
             <p><strong>Turno ID:</strong> ${turno.id}</p>
             <p><strong>Fecha del turno:</strong> ${turno.fecha}</p>
             <p><strong>Médico asignado:</strong> ${turno.medico.nombre} ${turno.medico.apellido}</p>
             <p><strong>Especialidad del Médico:</strong> ${turno.medico.especialidad.nombre}</p>
-            <button class="cancelarBoton" onclick="api_cancelarTurno(${turno.id})">Cancelar turno</button>
+            <button class="cancelarBoton" onclick="cancelarTurno(${turno.id})">Cancelar turno</button>
 
         `;
         ul.appendChild(li);  // Añadimos el LI a la lista UL
     });
 }
+
+
+function cancelarTurno(turnoId){
+    pop1("¿Desea cancelar el turno?").then((result) => {
+        if (result) {
+            console.log("cancelando turno " + turnoId);
+            api_cancelarTurno(turnoId);
+        }else{
+            console.log("salir");
+        }
+    });
+        
+}
+
+buscarTurnos();
