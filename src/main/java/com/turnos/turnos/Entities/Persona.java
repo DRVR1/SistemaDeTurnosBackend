3e711bed6f4fac218723775e6b57abd8afdfbade
Ian Vidmar <68394559+DRVR1@users.noreply.github.com>
2024-10-20 18:58:23 -0300

    Dni unico

--- src/main/java/com/turnos/turnos/Entities/Persona.java
+++ src/main/java/com/turnos/turnos/Entities/Persona.java
@@ -15,7 +15,7 @@ public abstract class Persona {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     protected String dni;
 
     @Column(nullable = false)
