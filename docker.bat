d0322c5a2dd1189984472fdf3ecda9959c089f09
Ian Vidmar <68394559+DRVR1@users.noreply.github.com>
2024-10-20 11:49:15 -0300

    Create docker.bat

---
+++ docker.bat
@@ -0,0 +1,31 @@
+powershell -Command "docker ps"
+
+powershell -Command "docker stop turnosbackend"
+powershell -Command "docker stop sistemadeturnosbackend-sql-server-1"
+
+powershell -Command "docker rm sistemadeturnosbackend-app-1"
+powershell -Command "docker rm sistemadeturnosbackend-sql-server-1"
+
+powershell -Command "docker images"
+
+powershell -Command "docker rmi turnosbackend"
+
+powershell -Command "docker build -t turnosbackend ."
+
+powershell -Command "docker-compose up -d"
+
+echo =======^
+=======^
+Cuando la base de datos se este ejecutando, presionar enter.^
+=======^
+=======
+
+pause
+
+sqlcmd -S localhost -U sa -P YourStrong@Passw0rd -Q "CREATE DATABASE turnos;"
+
+powershell -Command "docker stop turnosbackend"
+powershell -Command "docker stop sistemadeturnosbackend-sql-server-1"
+
+powershell -Command "docker-compose up -d"
+
