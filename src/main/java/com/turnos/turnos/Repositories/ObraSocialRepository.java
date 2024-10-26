7bfcc318f907c1d634c98a21cd862a77804ab6a8
Tassi <tassi.tbj@gmail.com>
2024-10-26 13:00:40 -0300

    borro todo lo de mi rama para empazar denuevo

--- src/main/java/com/turnos/turnos/Repositories/ObraSocialRepository.java
+++
@@ -1,9 +0,0 @@
-package com.turnos.turnos.Repositories;
-
-import com.turnos.turnos.Entities.ObraSocial;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ObraSocialRepository extends JpaRepository<ObraSocial, Long> {
-}
